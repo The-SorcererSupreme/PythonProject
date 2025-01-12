cf9a70e99946540482f756b1c644edb341d3acdd
The-SorcererSupreme <rollinger.christophe@gmail.com>
2025-01-12 13:33:06 +0000

    Safe upload before upgrading angular

--- frontend/python-project/src/app/app.config.ts
+++ frontend/python-project/src/app/app.config.ts
@@ -5,10 +5,17 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG(({
+      ripple: true
+  })),
   ]
 };
